1507dd201367acf4e7e33e578838c8a91af302b2
ItsCharzion <106410330+ItsCharzion@users.noreply.github.com>
2023-02-02 02:41:08 -0500

    e

--- src/commands/Community/ticket.js
+++ src/commands/Community/ticket.js
@@ -1,78 +1,44 @@
-const { Client,  EmbedBuilder, PermissionsBitField, Permissions, MessageManager, Embed, Events, GuildMember, GuildHubType, AuditLogEvent, Intents, Collection, GatewayIntentBits } = require('discord.js');
-const fs = require('fs');
-const client = new Client({ intents: [Object.keys(GatewayIntentBits)] }); 
+const {SlashCommandBuilder} = require('@discordjs/builders')
+const {EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle, PermissionsBitField, permissionOverwites, ButtonInteraction, ChannelType } = require('discord.js');
 
-client.commands = new Collection();
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName('ticket')
+    .setDescription('The ticket Command'),
+    async execute (interaction, client) {
 
-require('dotenv').config();
+        if(!interaction.member.permissions.has(PermissionsBitField.Flags.Administrator)) return await interaction.reply({content: 'You cant do that, dumb fuck'});
+        
+        const button = new ActionRowBuilder()
+        .addComponents(
+            new ButtonBuilder()
+            .setCustomId('button')
+            .setEmoji('📩')
+            .setLabel('Purchase/Contact')
+            .setStyle(ButtonStyle.Secondary),
+        )
+        const embed = new EmbedBuilder()
+        .setColor("Blue")
+        .setTitle("Tickets & Purchases")
+        .setDescription('Click the button below to make a purchase or contact us.')
 
-const functions = fs.readdirSync("./src/functions").filter(file => file.endsWith(".js"));
-const eventFiles = fs.readdirSync("./src/events").filter(file => file.endsWith(".js"));
-const commandFolders = fs.readdirSync("./src/commands");
+        await interaction.reply({ embeds: [embed], components: [button]});
 
-(async () => {
-    for (file of functions) {
-        require(`./functions/${file}`)(client);
+        const collector = await interaction.channel.createMessageComponentCollector();
+        collector.on('collect', async i => {
+
+            await i.update({ embeds: [embed], components: [button] });
+
+            const channel = await interaction.guild.channels.create({
+                name: `ticket ${i.user.tag}`,
+                type: ChannelType.GuildText,
+                parent: '1068569975650013225'
+            });
+            channel.permissionOverwrites.create(i.user.id, { ViewChannel: true, SendMessages: true} );
+            channel.permissionOverwites.create(channel.guild.roles.everyone, {ViewChannel: false, SendMessages: false});
+
+            channel.sent({content: `Welcome to your ticket, ${i.user}. Staff will arrive shortly to assist you with your purchase.`})
+            i.user.send(`Your Ticket in ${i.guild.name} has been created.`)
+        })
     }
-    client.handleEvents(eventFiles, "./src/events");
-    client.handleCommands(commandFolders, "./src/commands");
-    client.login(process.env.token)
-})();
-
-//Mod Log System
-//client.on(Events.MessageDelete, async message => {
-
-    //message.guild.fetchAuditLogs({
-        //type: AuditLogEvent.MessageDelete,
-    //})
-    //.then(async audit => {
-        //const { executor } = audit.entries.last()
-
-        //const mes = message.content;
-
-        //if (!mes) return;
-
-        //const channelID = '898293748121862215';
-        //const mchannel = await message.guild.channels.cache.get(channelID);
-
-        //const embed = new EmbedBuilder()
-        //.setColor("Red")
-        //.setTitle("Message Deleted")
-        //.addFields({ name: "Message Content", value: `${mes}`, inline: false})
-        //.addFields({ name: "Message Channel", value: `${message.channel}`, inline: false})
-        //.addFields({ name: "Deleted By", value: `${executor.tag}`, inline: false})
-        //.setTimestamp()
-        //.setFooter({ text: "Mod Logging System"})
-
-        //mchannel.send({ embeds: [embed] })
-    //})
-//})
-
-
-if(command === 'calculator') {
-
-    const { ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
-    const button = new ActionRowBuilder()
-    .addComponents(
-        new ButtonBuilder()
-        .setCustomId('1')
-        .setLabel('1')
-        .setStyle(ButtonStyle.Primary),
-    );
-
-    const embed = new EmbedBuilder()
-    .setColor("Blue")
-    .setDescription(`${message.author.tag}'s calcultor!`)
-
-    const embed2 = new EmbedBuilder()
-    .setColor("1! You pressed 1.")
-
-    message.channel.send({ embeds: [embed], components: [button]});
-
-    const collector = message.channel.createMessageComponentCollector() ;
-
-    collector.on('collect', async i => {
-        await i.update({ embeds: [embed2], components: [button]})
-    })
-
 }
